48d6cd2db869b2b52a77c1d2c7a7210a11238641
athan37 <athan@bates.edu>
2024-04-26 18:00:03 -0400

    Initial commit

---
+++ frontend/src/components/pages/business-main/index.jsx
@@ -0,0 +1,8 @@
+// import styles from "./styles.module.css";
+
+// export default function BusinessMain() {
+
+
+//     return (
+//     )
+// }
--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,10 +1,15 @@
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 import WalletProvider from '@/components/providers/wallet'
 import '@/styles/globals.css'
 
+
 export default function App({ Component, pageProps }) {
   return (
-    <WalletProvider>
-      <Component {...pageProps} />
-    </WalletProvider>
+    <ChakraProvider>
+      <WalletProvider>
+        <Component {...pageProps} />
+      </WalletProvider>
+    </ChakraProvider>
   )
 }
